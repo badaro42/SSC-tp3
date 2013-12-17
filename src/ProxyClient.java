af1fb0305f81cc6cd176de187e2c2a57f8285b6e
Rafael Bizarra <r.bizarra@campus.fct.unl.pt>
2013-12-17 13:04:55 +0000

    Limpeza ProxyClient3

--- src/ProxyClient.java
+++ src/ProxyClient.java
@@ -68,12 +68,7 @@ class ProxyClient {
         noncesControl = new HashSet<byte[]>();
         md = MessageDigest.getInstance("SHA-1");
 
-        dfEnc = Cipher.getInstance("AES/CFB/PKCS5Padding", "BC");
-        dfDec = Cipher.getInstance("AES/CFB/PKCS5Padding", "BC");
-
         socket = new DatagramSocket();
-
-        initAuthCipher();
 
         authenticate(user, password);
 
@@ -116,15 +111,10 @@ class ProxyClient {
             cipher = Cipher.getInstance(cipherName, provider);
             hMac = Mac.getInstance(hmacType, provider);
 
-
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | NoSuchProviderException e) {
-            e.printStackTrace();
-        }
-
-        try {
             cipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
             hMac.init(hMacKey);
-        } catch (InvalidKeyException | InvalidAlgorithmParameterException e) {
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -218,7 +208,6 @@ class ProxyClient {
         }
     }
 
-
     //certificado
     //TODO gerar certificados para o cliente e servidor
 
@@ -235,7 +224,6 @@ class ProxyClient {
     private KeyManagerFactory clientKM = null;
     private TrustManagerFactory serverTrustManager = null;
 
-	
     FileInputStream input = null;
     private KeyStore ks = null;
 
@@ -292,123 +280,6 @@ class ProxyClient {
         return (new String[3]);
     }
 
-
-    //TODO
-    byte[] nonceC;
-    byte[] nonceS;
-    InetAddress authsocket;
-    KeyAgreement sKeyAgree;
-    KeyPair sPair;
-    KeyPairGenerator keyGen;
-    // Parametro para o gerador do Grupo de Cobertura de P
-    private BigInteger g512 = new BigInteger(
-            "153d5d6172adb43045b68ae8e1de1070b6137005686d29d3d73a7" + "749199681ee5b212c9b96bfdcfa5b20cd5e3fd2044895d609cf9b"
-                    + "410b7a0f12ca1cb9a428cc", 16);
-
-    // Um grande numero primo P
-    private BigInteger p512 = new BigInteger(
-            "9494fec095f3b85ee286542b3836fc81a5dd0a0349b4c239dd387" + "44d488cf8e31db8bcb7d33b41abb9e5a33cca9144b1cef332c94b"
-                    + "f0573bf047a3aca98cdf3b", 16);
-    private DHParameterSpec dhParams = new DHParameterSpec(p512, g512);
-
-    SecureRandom srNonces;
-
-    private void initAuthCipher() throws Exception {
-
-        srNonces = SecureRandom.getInstance("SHA1PRNG");
-
-        authsocket = InetAddress.getByName(AUTH_SERVER);
-
-        keyGen = KeyPairGenerator.getInstance("DH", "BC");
-
-        SecureRandom sr = new SecureRandom();
-
-//				keyGen.initialize(dhParams, UtilsDH.createFixedRandom());
-        keyGen.initialize(dhParams, sr);
-
-        sKeyAgree = KeyAgreement.getInstance("DH", "BC");
-        sPair = keyGen.generateKeyPair();
-
-
-        sKeyAgree.init(sPair.getPrivate());
-    }
-
-    private byte[] addHeader(byte[] msg, int protVersion, int contType) {
-        msg[0] = (byte) protVersion;
-        msg[1] = (byte) contType;
-        msg[2] = (byte) ((msg.length - 4) >>> 8);
-        msg[3] = (byte) (msg.length - 4);
-        return msg;
-    }
-
-    private byte[] addHeader(byte[] msg, int protVersion, int contType, int length) {
-        msg[0] = (byte) protVersion;
-        msg[1] = (byte) contType;
-        msg[2] = (byte) (length - 4 >>> 8);
-        msg[3] = (byte) (length - 4);
-        return msg;
-    }
-
-    private void sendMsgType6(String username) {
-        try {
-            byte[] key = sPair.getPublic().getEncoded();
-
-            byte[] msg = new byte[65536];
-            msg = addHeader(msg, PROT_VERSION, 6);
-            int offset = 4;
-            msg[offset++] = (byte) ((key.length) >>> 8);
-            msg[offset++] = (byte) (key.length);
-
-            //username hash
-            byte[] usernameBytes = username.getBytes();
-            md.update(usernameBytes);
-            byte[] usernameHash = md.digest();
-            msg[offset++] = (byte) ((usernameHash.length) >>> 8);
-            msg[offset++] = (byte) (usernameHash.length);
-
-
-            //nonce
-            nonceC = new byte[NONCE_LENGTH];
-            srNonces.nextBytes(nonceC);
-
-            //first cipher
-            byte[] toCipher_PubServer = new byte[NONCE_LENGTH + usernameBytes.length];
-            System.arraycopy(nonceC, 0, toCipher_PubServer, 0, NONCE_LENGTH);
-            System.arraycopy(usernameBytes, 0, toCipher_PubServer, NONCE_LENGTH, usernameBytes.length);
-            byte[] ciphered_pubServer = serverAsymCipher.doFinal(toCipher_PubServer);
-            msg[offset++] = (byte) ((ciphered_pubServer.length) >>> 8);
-            msg[offset++] = (byte) (ciphered_pubServer.length);
-
-
-            //second cipher
-            byte[] toCipher_PrivUser = new byte[NONCE_LENGTH + usernameBytes.length];
-            byte[] ciphered_privUser = clientAsymCipher.doFinal(toCipher_PrivUser);
-            msg[offset++] = (byte) ((ciphered_privUser.length) >>> 8);
-            msg[offset++] = (byte) (ciphered_privUser.length);
-
-            //copy data to msg
-            System.arraycopy(key, 0, msg, offset, key.length);
-            offset += key.length;
-            System.arraycopy(usernameHash, 0, msg, offset, usernameHash.length);
-            offset += usernameHash.length;
-            System.arraycopy(ciphered_pubServer, 0, msg, offset, ciphered_pubServer.length);
-            offset += ciphered_pubServer.length;
-            System.arraycopy(ciphered_privUser, 0, msg, offset, ciphered_privUser.length);
-            offset += ciphered_privUser.length;
-
-
-            DatagramPacket p = new DatagramPacket(msg, 0, offset);
-            p.setAddress(authsocket);
-            p.setPort(AUTH_SERVER_PORT);
-
-
-            socket.send(p);
-        } catch (Exception e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-
-    }
-
     private byte[] getContent(byte[] msg) {
         byte[] aux = {msg[2], msg[3]};
         int contentLength = new BigInteger(1, aux).intValue();
@@ -417,252 +288,8 @@ class ProxyClient {
         return content;
     }
 
-    private void finishDHKey(byte[] bKey) {
-        KeyFactory kf = null;
-        try {
-            kf = KeyFactory.getInstance("DH");
-            X509EncodedKeySpec x509Spec = new X509EncodedKeySpec(bKey);
-            PublicKey clientPublicKey = kf.generatePublic(x509Spec);
-
-            sKeyAgree.doPhase(clientPublicKey, true);
-
-            MessageDigest hash = MessageDigest.getInstance("SHA1", "BC");
-
-            // Then A generates the final agreement key
-            dfKey = hash.digest(sKeyAgree.generateSecret());
-            byte[] ivBytes = stringToBytes("f5m8sj9c7lwq5tk5y7ti6ikgn");
-
-            byte[] toUse = new byte[16];
-            System.arraycopy(dfKey, 0, toUse, 0, 16);
-
-            SecretKeySpec key = new SecretKeySpec(toUse, "AES");
-            dfDec.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(ivBytes));
-            dfEnc.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(ivBytes));
-
-//			System.out.println("dfKey = " + new String(dfKey));
-//			System.out.println("++++++------------+++++++");
-//			for(byte b: dfKey)
-//				System.out.print(b + " ");
-//			System.out.println("++++++-----------------+++++++");
-
-
-        } catch (Exception e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
-
-
-    private boolean processMsgType7(byte[] packet) {
-        try {
-            byte[] msg = getContent(packet);
-            int length = msg.length;
-            byte[] aux = {msg[0], msg[1]};
-            int keyLength = new BigInteger(1, aux).intValue();
-
-//			System.out.println("MSG");
-//			for (byte b: msg)
-//				System.out.print(b + " ");
-//			System.out.println("----");
-
-
-            int offset = 2;
-            byte[] auxNonce = new byte[NONCE_LENGTH];
-            System.arraycopy(msg, offset, auxNonce, 0, NONCE_LENGTH);
-            offset += auxNonce.length;
-            if (noncesControl.contains(auxNonce)) {
-                return false;
-            }
-            nonceC[nonceC.length - 1] += 1;
-
-
-            if (!MessageDigest.isEqual(auxNonce, nonceC)) {
-                nonceC[nonceC.length - 1] -= 1;
-                return false;
-            }
-
-
-            nonceS = new byte[NONCE_LENGTH];
-            System.arraycopy(msg, offset, nonceS, 0, NONCE_LENGTH);
-            offset += NONCE_LENGTH;
-
-            byte[] key = new byte[keyLength];
-            System.arraycopy(msg, offset, key, 0, keyLength);
-            offset += keyLength;
-
-            //calc auxHash
-            md.update(msg, 2, offset);
-            byte[] auxHash = md.digest();
-
-            byte[] toDecipher = new byte[length - offset];
-            System.arraycopy(msg, offset, toDecipher, 0, toDecipher.length);
-            byte[] plainData = serverAsymCipher.doFinal(toDecipher);
-
-            if (!MessageDigest.isEqual(auxHash, plainData))
-//			return false;
-                ;
-
-
-            finishDHKey(key);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-
-        return true;
-    }
-
-
-    private void sendMsgType8(InetAddress adddres, int port) {
-        try {
-            byte[] msg = new byte[65536];
-            int msgLength = 4;
-
-            //generate new Nonce
-            srNonces.nextBytes(nonceC);
-            System.arraycopy(nonceC, 0, msg, msgLength, nonceC.length);
-            msgLength += nonceC.length;
-
-
-            nonceS[nonceS.length - 1] += 1;
-            System.arraycopy(nonceS, 0, msg, msgLength, nonceS.length);
-            msgLength += nonceS.length;
-
-            md.update(msg, 4, NONCE_LENGTH * 2);
-            byte[] hash = md.digest();
-
-            byte[] cipheredHash = dfEnc.doFinal(hash);
-            System.arraycopy(cipheredHash, 0, msg, msgLength, cipheredHash.length);
-            msgLength += cipheredHash.length;
-
-
-            msg = addHeader(msg, PROT_VERSION, 8, msgLength);
-            DatagramPacket dp = new DatagramPacket(msg, 0, msgLength);
-            dp.setAddress(adddres);
-            dp.setPort(port);
-
-            try {
-                socket.send(dp);
-            } catch (IOException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private boolean processMsgType9(byte[] packet) {
-        try {
-
-
-            byte[] msg = getContent(packet);
-            int length = msg.length;
-
-            byte[] aux = {msg[0], msg[1]};
-            int sessionCPsize = new BigInteger(1, aux).intValue();
-            byte[] aux2 = {msg[2], msg[3]};
-            int cipheredKeySize = new BigInteger(1, aux2).intValue();
-
-            int initOffset = 4;
-            int offset = initOffset;
-
-
-            String sessionCP = new String(msg, offset, sessionCPsize);
-            offset += sessionCPsize;
-
-
-            byte[] cipheredStreamKey = new byte[cipheredKeySize];
-            System.arraycopy(msg, offset, cipheredStreamKey, 0, cipheredKeySize);
-            offset += cipheredKeySize;
-            byte[] streamKey = dfDec.doFinal(cipheredStreamKey);
-//			byte[]	streamKey = cipheredStreamKey;
-
-
-            nonceC[nonceC.length - 1] += 1;
-            byte[] auxNonce = new byte[NONCE_LENGTH];
-            System.arraycopy(msg, offset, auxNonce, 0, NONCE_LENGTH);
-            if (!MessageDigest.isEqual(nonceC, auxNonce)) {
-                nonceC[nonceC.length - 1] -= 1;
-                return false;
-            }
-            offset += NONCE_LENGTH;
-
-            System.arraycopy(msg, offset, auxNonce, 0, NONCE_LENGTH);
-            if (noncesControl.contains(auxNonce)) {
-                nonceC[nonceC.length - 1] -= 1;
-                return false;
-            }
-            offset += NONCE_LENGTH;
-
-            byte[] msgHash = new byte[length - offset];
-            System.arraycopy(msg, offset, msgHash, 0, length - offset);
-
-
-            //generate Hash
-            byte[] toHash = new byte[sessionCPsize + 2 * NONCE_LENGTH + streamKey.length];
-            int hashOffset = 0;
-            System.arraycopy(msg, initOffset, toHash, hashOffset, sessionCPsize);
-            hashOffset += sessionCPsize;
-            System.arraycopy(nonceC, 0, toHash, hashOffset, NONCE_LENGTH);
-            hashOffset += NONCE_LENGTH;
-            System.arraycopy(auxNonce, 0, toHash, hashOffset, NONCE_LENGTH);
-            hashOffset += NONCE_LENGTH;
-            System.arraycopy(streamKey, 0, toHash, hashOffset, streamKey.length);
-            hashOffset += streamKey.length;
-            md.update(toHash);
-            byte[] hash = md.digest();
-            if (!MessageDigest.isEqual(hash, msgHash)) {
-                //nonceC[nonceC.length-1] -= 1;
-                //return false;
-            }
-
-            initCipherSuite(sessionCP, streamKey);
-
-
-            noncesControl.add(auxNonce);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return true;
-    }
-
+    //TODO
     private void authenticate(String username, String password) throws Exception {
-
-        initAuthCipher();
-
-        byte[] buffer = new byte[65536];
-        DatagramPacket dgPacket = new DatagramPacket(buffer, buffer.length);
-
-        socket.setSoTimeout(7000);
-        do {
-            try {
-                sendMsgType6(username);
-
-                socket.receive(dgPacket);
-
-                processMsgType7(dgPacket.getData());
-
-                InetAddress address = dgPacket.getAddress();
-                int port = dgPacket.getPort();
-
-                sendMsgType8(address, port);
-
-                socket.receive(dgPacket);
-
-                break;
-            } catch (SocketTimeoutException te) {
-                continue;
-            }
-        } while (true);
-
-        processMsgType9(dgPacket.getData());
-
-
-//		if (processMsgType7())
-
 
     }
 
@@ -670,9 +297,7 @@ class ProxyClient {
     static public void main(String[] args) throws Exception {
         if (args.length != 2) {
             System.out.println("Use: ProxyClient user password");
-
         }
-
 
         System.out.println("Authenticating...");
         ProxyClient pc = new ProxyClient(args[0], args[1]);
